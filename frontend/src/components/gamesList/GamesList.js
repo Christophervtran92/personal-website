37b3491e8105377bc7c4e44b8301fb8b97b27a50
Christophervtran92 <christophervtran92@gmail.com>
2022-03-20 17:47:05 -0700

    Added functionality to update existing games in the DB

--- frontend/src/components/gamesList/GamesList.js
+++ frontend/src/components/gamesList/GamesList.js
@@ -4,10 +4,11 @@ import NavBar from '../navBar/NavBar'
 import axios from "axios";
 import '../home/Home.css'
 import './GamesList.css'
-import {Button, Table, Tab, Tabs, ButtonGroup, ToggleButton, InputGroup, Form, FormControl, Col, Row, Modal} from 'react-bootstrap';
+import {Button, Table, Tab, Tabs, ButtonGroup, ToggleButton, InputGroup, Form, FormControl, Col, Row, Modal, Toast} from 'react-bootstrap';
 
 const headings = ["Title", "System", "Release Date", "Status", "Year Completed"];
 
+/*
 function UpdateModal(props) {
     return (
       <Modal style={{fontFamily: "Quicksand"}}
@@ -22,7 +23,7 @@ function UpdateModal(props) {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <Form>
+            <Form id="formData">
                 <Form.Group className="update-title" controlId="form-update-title">
                     <Form.Label>Title</Form.Label>
                     <Form.Control type="text" name="title" placeholder={props.responseB[0].title}/>
@@ -46,12 +47,13 @@ function UpdateModal(props) {
             </Form>
         </Modal.Body>
         <Modal.Footer>
-            <Button variant="success">Update</Button>
+            <Button variant="success" type="submit" form="formData">Update</Button>
             <Button variant="secondary" onClick={props.onHide}>Close</Button>
         </Modal.Footer>
       </Modal>
     );
   }
+  */
 
 export default function GamesList() {
 
@@ -59,7 +61,6 @@ export default function GamesList() {
     const [responseB, setResponseB] = useState([{title: "Title", system: "System"}]);
     const [userInput, setUserInput] = useState([]);
     const [userInputB, setUserInputB] = useState("");
-    const [formData, setFormData] = useState({title: "temp", system: "temp"});
     const [filter, setFilter] = useState([]);
     const [selection, setSelection] = useState([]);
     const radio_btn = [
@@ -68,6 +69,7 @@ export default function GamesList() {
         {name: 'delete', value: 'delete'}
     ];
     const [modalShow, setModalShow] = React.useState(false);
+    var formData = ["title", "system", "release-date", "status", "yr_completed"];
 
     useEffect(() => {
         if(filter && userInput) {
@@ -138,11 +140,77 @@ export default function GamesList() {
                 }
             })
             .catch(err => {
+                console.log("Bad input")
                 err.status(400).json(err)
             })
         }
     }
 
+    //Takes the data inputted from the modal and converts it over to a JSON object which then is used to update the info in the DB using axios
+    const handleUpdateSubmit=(e)=>{
+        const updateObj = {title: formData[0], system: formData[1], release_date: formData[2], status: formData[3], yr_completed: formData[4]};
+
+        axios.put("http://localhost:4000/games/"+responseB[0]._id, updateObj)
+        .then(res => {
+            console.log(res.data)
+            alert("updated to:\ntitle: " + updateObj.title + "\nsystem: " + updateObj.system + "\nrelease date: " + updateObj.release_date + "\nstatus: " + updateObj.status + "\nyear completed: " + updateObj.yr_completed);
+        })
+        .catch(err => {
+            console.log("Bad Request")
+            err.status(400).json(err)
+        })
+    }
+
+    function UpdateModal(props) {
+        return (
+          <Modal style={{fontFamily: "Quicksand"}}
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Update Menu
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body onLoad={formData[0] = props.responseB[0].title,
+                                formData[1] = props.responseB[0].system,
+                                formData[2] = props.responseB[0].release_date,
+                                formData[3] = props.responseB[0].status,
+                                formData[4] = props.responseB[0].yr_completed}>
+                <Form id="update-form">
+                    <Form.Group className="update-title" controlId="form-update-title">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" name="title" defaultValue={props.responseB[0].title} onChange={(e)=>formData[0] = e.target.value}/>
+                    </Form.Group>
+                    <Form.Group className="update-system" controlId="form-update-system">
+                        <Form.Label>System</Form.Label>
+                        <Form.Control type="text" name="system" defaultValue={props.responseB[0].system} onChange={(e)=>formData[1] = e.target.value}/>
+                    </Form.Group>
+                    <Form.Group className="update-release-date" controlId="form-update-release-date">
+                        <Form.Label>Release Date</Form.Label>
+                        <Form.Control type="text" name="release_date" defaultValue={props.responseB[0].release_date } onChange={(e)=>formData[2] = e.target.value}/>
+                    </Form.Group>
+                    <Form.Group className="update-status" controlId="form-update-status">
+                        <Form.Label>Status</Form.Label>
+                        <Form.Control type="text" name="status" defaultValue={props.responseB[0].status} onChange={(e)=>formData[3] = e.target.value}/>
+                    </Form.Group>
+                    <Form.Group className="update-yr-completed" controlId="form-update-yr-completed">
+                        <Form.Label>Year Completed</Form.Label>
+                        <Form.Control type="number" name="yr_completed" defaultValue={props.responseB[0].yr_completed} onChange={(e)=>formData[4] = e.target.value}/>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="success" onClick={handleUpdateSubmit}>Update</Button>
+                <Button variant="secondary" onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        );
+      }
+
+      
     const gamesListTable = () => {
         return response.map((res, index) => {
             switch(res.status) {
@@ -281,6 +349,7 @@ export default function GamesList() {
                                         </Button>
                                         <UpdateModal
                                             responseB={responseB}
+                                            formData={formData}
                                             show={modalShow}
                                             onHide={() => setModalShow(false)}
                                         />
